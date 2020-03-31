3303e9c601f22f1e24eebfe23255b995fd5a49b5
echizenya <h.fqui35@gmail.com>
2020-03-31 14:19:37 +0900

    AMP の更新

--- wp-content/plugins/amp/vendor/autoload.php
+++ wp-content/plugins/amp/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit89d25d6c150c693f89f827879baa004d::getLoader();
+return ComposerAutoloaderInit08a5547e33fe7d0cd45f61c354a5a208::getLoader();
