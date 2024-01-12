78828ebb00e93178c519f490ea01c8e3b9f60456
echizenya <h.fqui35@gmail.com>
2024-01-12 22:43:53 +0900

    AMP の更新

--- wp-content/plugins/amp/vendor/composer/installed.php
+++ wp-content/plugins/amp/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ampproject/amp-wp',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => 'fa29952e8cc9f018b427dec82ebf16cdd82a5983',
+        'reference' => '716c4188a9ee39e76222c2e20e16a748d93e4775',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'ampproject/amp-wp' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => 'fa29952e8cc9f018b427dec82ebf16cdd82a5983',
+            'reference' => '716c4188a9ee39e76222c2e20e16a748d93e4775',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
